376bf9098d602f0bb237e2b6eb718dcc24754979
unknown <manuel_odili@yahoo.com>
2022-03-22 22:07:49 +0000

    first commit description

---
+++ log1.py
@@ -0,0 +1,9 @@
+import os
+
+location = '/Users/Damilola Odili/Downloads/VMware Research Collaboration Project/gclog/async-replicator/'
+for filename in os.listdir(location):
+    if filename == 'gc.log.1':
+       f  = open(os.path.join(location, 'gc.log.1'), "r")
+       print (f.read())
+
+
